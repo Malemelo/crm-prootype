558d25d4bfecb697d86ae5736c0bfc9538fed9f1
Your Name <desireirankundawilliams@gmail.com>
2021-06-08 21:51:58 +0200

    added a delete and an update route

--- backend/controllers/routes/user.js
+++ backend/controllers/routes/user.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 
 
-// get list of all the users
+// get list of all the users 
 router.get('/users/:id?', async (req, res) => {
     if (req.params.id) {
         const user = await UserModel.findOne({ _id: req.params.id })
@@ -15,6 +15,35 @@ router.get('/users/:id?', async (req, res) => {
     } else {
         const users = await UserModel.find({})
         res.status(200).send({ users });
+    }
+
+});
+
+// updating users
+router.put('/users', async (req, res) => {
+    try {
+        const updatedUser = await UserModel.updateOne({ _id: req.body._id }, { ...req.body })
+        if (!updatedUser) {
+            res.status(400).json({ "error": 'Something went wrong' })
+        }
+        res.status(200).send({ user: updatedUser });
+    } catch (error) {
+        res.status(400).json({ "error": error })
+    }
+
+});
+
+// delete user by id
+
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const deleteUser = await UserModel.deleteOne({ _id: req.params.id })
+        if (!deleteUser) {
+            res.status(404).json({ "error": 'User Not found' })
+        }
+        res.status(200).send({ deleteUser });
+    } catch (error) {
+            res.status(400).send({error});
     }
 
 });
